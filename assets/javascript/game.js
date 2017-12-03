7c1562f73a00c2cf596cfcb887400ca7c51af8d3
Eric Traband <ejstraband@gmail.com>
2017-12-03 14:37:26 -0600

    starting on lettermatch functions. Basic UI up with a few small bugs.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
-// Pick the word, determine the length, store gu
+
+// variables for the javascript
 
 var secretWord = "xyz";
 var secretWordLength = secretWord.length;
-var userGuesseses = [];
+var wrongGuesses = [];
 var userSuccesses = [];
-var userSolution = [];
+var currentWord = [];
 var guessesRemaining = 0;
+var wins = 0;
+var losses = 0;
 
-
+// variables for the HTML writes
+var htmlWins = document.getElementById('wins');
+var htmlLosses = document.getElementById('losses');
+var htmlCurrentWord = document.getElementById('currentWord');
 var htmlGuessesRemaining = document.getElementById('guessesRemaining');
+var htmlWrongGuesses = document.getElementById('wrongGuesses');
+
 
 console.log("Initial Declaration of GuessesRemaining (should be Zero): " + guessesRemaining);
 
@@ -18,13 +26,18 @@ function appSetup() {
 	secretWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 	secretWordLength = secretWord.length;
 	guessesRemaining = 13
+	currentWord = [];
 	console.log("Reset of GuessesRemaining (should be 13): " + guessesRemaining);	
 }
 
 function appStart() {
 	console.log("The secret word is " + secretWord);
 	console.log("The secret word is " + secretWordLength + " letters long.")
-	console.log("Let's Get Started! Push a key to start guessing.");
+	alert("Let's Get Started! Push a key to start guessing.");
+}
+
+function letterMatch() {
+	
 }
 
 appSetup();
@@ -36,17 +49,19 @@ document.onkeyup = function(event) {
 	var userGuesses = event.key;
 	// while you have guesses remaining
 	if (guessesRemaining > 1) {
-		userSolution.push(userGuesses);
-		console.log(userSolution);
+		currentWord.push(userGuesses);
+		console.log(currentWord);
 		console.log(userGuesses);
 		guessesRemaining = (guessesRemaining - 1);
 		console.log("Guesses Remaining: " + guessesRemaining);
+		htmlWrongGuesses = wrongGuesses;
 		htmlGuessesRemaining.innerHTML = guessesRemaining;
-		console.log("htmlGuessesRemaining: " + htmlGuessesRemaining)
-		}
-
-	else if (guessesRemaining === 1) {
+	} else {
+		htmlGuessesRemaining.innerHTML = "none";
+		losses++;
+		htmlLosses.innerHTML = losses;
 		console.log("You Lose!");
+		alert("You Lose!");
 		appSetup();
 		appStart();
 	}
