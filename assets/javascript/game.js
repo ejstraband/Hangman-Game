4c32a9c37e7c869bfb70997b962d37f692f6aba6
Eric Traband <ejstraband@gmail.com>
2017-12-03 13:32:22 -0600

    got initial document.writes working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,54 @@
-var wordBank = ["amigos", "cactus", "eastwood", "gunfight", "locomotive", "mesas", "noose", "poker", "ranch", "saloon", "stagecoach", "tombstone", "wanted", "westworld"];
+// Pick the word, determine the length, store gu
 
-console.log("There are " + wordBank.length + " words in the bank.");
+var secretWord = "xyz";
+var secretWordLength = secretWord.length;
+var userGuesseses = [];
+var userSuccesses = [];
+var userSolution = [];
+var guessesRemaining = 0;
 
-var computerGuess = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-console.log("The computer word is " + computerGuess);
+var htmlGuessesRemaining = document.getElementById('guessesRemaining');
+
+console.log("Initial Declaration of GuessesRemaining (should be Zero): " + guessesRemaining);
+
+function appSetup() {
+	var wordBank = ["amigos", "cactus", "eastwood", "gunfight", "locomotive", "mesas", "noose", "poker", "ranch", "saloon", "stagecoach", "tombstone", "wanted", "westworld"];
+	console.log("There are " + wordBank.length + " words in the bank.");
+	secretWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	secretWordLength = secretWord.length;
+	guessesRemaining = 13
+	console.log("Reset of GuessesRemaining (should be 13): " + guessesRemaining);	
+}
+
+function appStart() {
+	console.log("The secret word is " + secretWord);
+	console.log("The secret word is " + secretWordLength + " letters long.")
+	console.log("Let's Get Started! Push a key to start guessing.");
+}
+
+appSetup();
+appStart();
 
 // main function
 document.onkeyup = function(event) {
 	// capture the user guess
-	var userGuess = event.key;
-
-	var guessesRemaining = 11;
-
-	if (guessesRemaining > 0) {
-
-		console.log(userGuess);	
-
-		guessesRemaining = (guessesRemaining - 1);
-
-		var userSolution = [];
-
-		console.log("Guesses Remaining: " + guessesRemaining);
-
-		userSolution.push(userGuess);
-
+	var userGuesses = event.key;
+	// while you have guesses remaining
+	if (guessesRemaining > 1) {
+		userSolution.push(userGuesses);
 		console.log(userSolution);
-
+		console.log(userGuesses);
+		guessesRemaining = (guessesRemaining - 1);
+		console.log("Guesses Remaining: " + guessesRemaining);
+		htmlGuessesRemaining.innerHTML = guessesRemaining;
+		console.log("htmlGuessesRemaining: " + htmlGuessesRemaining)
 		}
 
+	else if (guessesRemaining === 1) {
+		console.log("You Lose!");
+		appSetup();
+		appStart();
 	}
+
+}
