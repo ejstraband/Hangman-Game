a6580a12122d4acd98a2e70cd61ef393368a86c6
Eric Traband <ejstraband@gmail.com>
2017-12-03 20:51:12 -0600

    refactor v1 complete. Starting testing, but I want this version before I mess around more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
 
-// variables for the javascript
-
+// variables for the javascript.
 var secretWord = "xyz";
 var secretWordLength = secretWord.length;
 var wrongGuesses = [];
-var userSuccesses = [];
-var currentWord = [];
+var currentUserWord = [];
 var currentGuess = null;
 var masterGuesses = [];
 var guessesRemaining = 0;
@@ -15,10 +13,11 @@ var losses = 0;
 // variables for the HTML writes
 var htmlWins = document.getElementById('wins');
 var htmlLosses = document.getElementById('losses');
-var htmlCurrentWord = document.getElementById('currentWord');
+var htmlcurrentUserWord = document.getElementById('currentUserWord');
 var htmlGuessesRemaining = document.getElementById('guessesRemaining');
 var htmlWrongGuesses = document.getElementById('wrongGuesses');
 
+// so many functions.... microservices, anyone? :-)
 
 // defines the word bank, picks the secret word, resets guesses, blanks user's current word
 function appSetup() {
@@ -27,28 +26,30 @@ function appSetup() {
 	secretWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 	secretWordLength = secretWord.length;
 	guessesRemaining = 13
-	currentWordBlanks();
+	currentUserWordBlanks();
 	console.log("Reset of GuessesRemaining (should be 13): " + guessesRemaining);
 	console.log("The secret word is " + secretWord);
 	console.log("The secret word is " + secretWordLength + " letters long.")
 	alert("Let's Get Started! Push a key to start guessing.");
 	}
 
-function currentWordBlanks() {
+// blanks user's current word
+function currentUserWordBlanks() {
 	for (var i = 0; i < secretWordLength; i++) {
-		currentWord.push(" _ ");
-		htmlCurrentWord.innerHTML = currentWord;
+		currentUserWord.push(" _ ");
+		htmlcurrentUserWord.innerHTML = currentUserWord;
     	}
 	}
 
+// Captures the user's guess
 function userGuessCapture() {
 	document.onkeyup = function(userGuess) {
-	// capture the user guess
 		currentGuess = userGuess.key;
 		isLetter(currentGuess);
 		}
 	}
 
+// verifies a guess is a letter and not a number or signal
 function isLetter() {
 	if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {
 		console.log("user typed " + currentGuess + ". This is a valid choice.");
@@ -58,14 +59,60 @@ function isLetter() {
 		}
 	}
 
+// attempts to match the letter
 function letterMatch() {
 	if ((secretWord.indexOf(currentGuess)) !== -1) {
-		console.log(currentGuess + " is one of the letters.")
+		letterMatched();
 		} else {
-      	console.log(currentGuess + " is NOT one of the letters.")
-      	wrongGuesses.push(currentGuess);
-      	htmlWrongGuesses.innerHTML = wrongGuesses;
+		letterNotMatched();
 		}
+	}
+
+function win() {
+	console.log("user won");
+	alert("Congrats, Partner!");
+	wins = wins++;
+	htmlWins.innerHTML = wins;
+	appSetup();
+}
+
+function lose() {
+	console.log("user lost");
+	alert("Bummer, y'all...");
+	htmlGuessesRemaining.innerHTML = "none";
+	losses = losses++;
+	htmlLosses.innerHTML = losses;
+	appSetup();
+}
+
+function guessOperation() {
+	userGuessCapture();
+	isLetter(currentGuess);
+}
+
+function letterMatched() {
+	console.log(currentGuess + " is one of the letters.")
+	checkIfAlreadyMatched(userGuess);
+	updateCurrentUserWord(userGuess);
+}
+
+function updateCurrentUserWord() {
+	currentUserWord.push(currentGuess);
+}
+
+function checkIfAlreadyMatched() {
+	if (((currentUserWord.indexOf(currentGuess)) !== -1) || ((currentUserWord.indexOf(wrongGuesses)) !== -1) ) {
+		alert("You already chose this letter. Please select another choice.");
+}
+
+function letterNotMatched() {
+	checkIfAlreadyMatched(userGuess);
+	console.log(currentGuess + " is NOT one of the letters.")
+	wrongGuesses.push(currentGuess);
+	wrongGuesses.sort();
+    htmlWrongGuesses.innerHTML = wrongGuesses;
+	guessesRemaining = (guessesRemaining - 1);
+	htmlGuessesRemaining.innerHTML = guessesRemaining;
 	}
 
 // main function primary logic
@@ -74,30 +121,26 @@ console.log("Initial Declaration of GuessesRemaining (should be Zero): " + guess
 appSetup();
 
 	// If my solution === secret word
+	if (currentUserWord === secretWord) {
 		// win
-	// else
-		// if guesses > 0
+		win();
+	} else {
+		if (guessesRemaining > 0) {
 			//guess
-		//else
+			guessOperation();
+		} else {
 			//lose
+			lose();
+		}
+	}
 
 
 
-// 	if ((guessesRemaining > 1)) {
-// 		currentWord.push(userGuesses);
-// 		console.log(currentWord);
-// 		console.log(userGuesses);
-// 		guessesRemaining = (guessesRemaining - 1);
-// 		console.log("Guesses Remaining: " + guessesRemaining);
-// 		htmlWrongGuesses = wrongGuesses;
-// 		htmlGuessesRemaining.innerHTML = guessesRemaining;
-// 	} else {
-// 		htmlGuessesRemaining.innerHTML = "none";
-// 		losses++;
-// 		htmlLosses.innerHTML = losses;
-// 		console.log("You Lose!");
-// 		alert("You Lose!");
-// 		appSetup();
-// 		appStart();
-// 	}
-// }
+
+	// if ((guessesRemaining > 1)) {
+
+	// 	currentUserWord.push(userGuesses);
+	// 	console.log(currentUserWord);
+	// 	console.log(userGuesses);
+
+	// 	console.log("Guesses Remaining: " + guessesRemaining);
